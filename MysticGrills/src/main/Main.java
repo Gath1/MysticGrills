05f6389baa07d8492383fe901f980bd75cbc93b2
Gath1 <103825298+Gath1@users.noreply.github.com>
2023-12-11 21:02:13 +0700

    Update Main.java
    sudah ada login and register

--- MysticGrills/src/main/Main.java
+++ MysticGrills/src/main/Main.java
@@ -1,14 +1,13 @@
 package main;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,17 +18,75 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Registration Tab Example");
+        primaryStage.setTitle("Login and Registration Example");
 
         TabPane tabPane = new TabPane();
-        Tab registrationTab = new Tab("Register");
 
+        // Login Tab
+        Tab loginTab = new Tab("Login");
+        GridPane loginGrid = createLoginForm();
+        loginTab.setContent(loginGrid);
+
+        // Registration Tab
+        Tab registrationTab = new Tab("Register");
+        GridPane registrationGrid = createRegistrationForm();
+        registrationTab.setContent(registrationGrid);
+
+        // Add tabs to the TabPane
+        tabPane.getTabs().addAll(loginTab, registrationTab);
+
+        // Set the login tab as the initial tab to show
+        tabPane.getSelectionModel().select(loginTab);
+
+        // Create a Register button to switch to the Registration tab
+        Button registerButton = new Button("Register");
+        registerButton.setOnAction(e -> Platform.runLater(() -> tabPane.getSelectionModel().select(registrationTab)));
+
+        // Create a Login button
+        Button loginButton = new Button("Login");
+        loginButton.setOnAction(e -> loginButtonClicked());
+
+        // Arrange buttons horizontally
+        HBox buttonBox = new HBox(10, loginButton, registerButton);
+        buttonBox.setPadding(new Insets(10));
+
+        // Create the main scene
+        VBox mainLayout = new VBox(tabPane, buttonBox);
+        Scene scene = new Scene(mainLayout, 400, 300);
+
+        // Set the stage's scene
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private GridPane createLoginForm() {
         GridPane grid = new GridPane();
         grid.setAlignment(javafx.geometry.Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
 
-        // Add UI components
+        // Add UI components for login
+        Label usernameLabel = new Label("Username:");
+        TextField usernameField = new TextField();
+        Label passwordLabel = new Label("Password:");
+        PasswordField passwordField = new PasswordField();
+
+        // Add components to the grid
+        grid.add(usernameLabel, 0, 0);
+        grid.add(usernameField, 1, 0);
+        grid.add(passwordLabel, 0, 1);
+        grid.add(passwordField, 1, 1);
+
+        return grid;
+    }
+
+    private GridPane createRegistrationForm() {
+        GridPane grid = new GridPane();
+        grid.setAlignment(javafx.geometry.Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+
+        // Add UI components for registration
         Label usernameLabel = new Label("Username:");
         TextField usernameField = new TextField();
         Label emailLabel = new Label("Email:");
@@ -38,7 +95,6 @@ public class Main extends Application {
         PasswordField passwordField = new PasswordField();
         Label confirmPasswordLabel = new Label("Confirm Password:");
         PasswordField confirmPasswordField = new PasswordField();
-        Button registerButton = new Button("Register");
 
         // Add components to the grid
         grid.add(usernameLabel, 0, 0);
@@ -49,20 +105,15 @@ public class Main extends Application {
         grid.add(passwordField, 1, 2);
         grid.add(confirmPasswordLabel, 0, 3);
         grid.add(confirmPasswordField, 1, 3);
-        grid.add(registerButton, 1, 4);
 
-        registerButton.setOnAction(e -> registerButtonClicked(usernameField.getText(), emailField.getText(), passwordField.getText(), confirmPasswordField.getText()));
-
-        registrationTab.setContent(grid);
-        tabPane.getTabs().add(registrationTab);
-
-        Scene scene = new Scene(tabPane, 400, 300);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        return grid;
     }
 
-    private void registerButtonClicked(String username, String email, String password, String confirmPassword) {
-        // Implement your registration logic here
-        System.out.println("Registration clicked with username: " + username + ", email: " + email + ", password: " + password + ", confirm password: " + confirmPassword);
+    private void loginButtonClicked() {
+        // Implement your login logic here
+        System.out.println("Login button clicked");
     }
 }
+
+
+
